00e64eb5874f615baf934fb05717a9002bbdab40
Gustavo Coronel <gcoronelc@gmail.com>
2021-05-07 21:52:36 -0500

    Unidad 02

---
+++ UNidad02/SQLQuery3.sql
@@ -0,0 +1,19 @@
+
+delete from BD57040..Customers;
+delete from BD57040..Territorios;
+delete from BD57040..Territorios2;
+
+
+select * from Northwind..Customers;
+go
+
+
+select * from BD57040..Customers;
+go
+
+select * from BD57040..Territorios;
+go
+
+select * from BD57040..Territorios2;
+go
+
