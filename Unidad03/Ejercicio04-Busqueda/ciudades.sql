c5ec076d3073f2e1fbe09c3f742540775327c6b1
Gustavo Coronel <gcoronelc@gmail.com>
2021-05-10 20:44:50 -0500

    Unidad 03

--- Unidad03/Ejercicio04-Busqueda/ciudades.sql
+++ Unidad03/Ejercicio04-Busqueda/ciudades.sql
@@ -7,15 +7,14 @@ descripcion varchar(50)
 )
 go
 
-insert into ciudades(idciudad,descripcion)
-values('AN','ANCASH')
-insert into ciudades(idciudad,descripcion)
-values('AQ','AREQUIPA')
-insert into ciudades(idciudad,descripcion)
-values('CJ','CAJAMARCA')
-insert into ciudades(idciudad,descripcion)
-values('CZ','CUZCO')
-insert into ciudades(idciudad,descripcion)
-values('HU','HUANUCO')
-insert into ciudades(idciudad,descripcion)
-values('LI','LIMA')
+insert into ciudades(idciudad,descripcion) values('AN','ANCASH')
+insert into ciudades(idciudad,descripcion) values('AQ','AREQUIPA')
+insert into ciudades(idciudad,descripcion) values('CJ','CAJAMARCA')
+insert into ciudades(idciudad,descripcion) values('CZ','CUZCO')
+insert into ciudades(idciudad,descripcion) values('HU','HUANUCO')
+insert into ciudades(idciudad,descripcion) values('LI','LIMA')
+go
+
+
+select * from ciudades;
+go
